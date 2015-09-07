b399b471f25e85cfc3a6cd86c54f56d0d1d951bd
Alfonso Saavedra "Son Link <sonlink.dourden@gmail.com>
2015-09-07 19:18:58 +0200

    Hola mundo y primer commit

---
+++ hola_mundo/hola_mundo/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_saludo = New System.Windows.Forms.Button()
+        Me.btn_salir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn_saludo
+        '
+        Me.btn_saludo.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_saludo.Location = New System.Drawing.Point(12, 12)
+        Me.btn_saludo.Name = "btn_saludo"
+        Me.btn_saludo.Size = New System.Drawing.Size(124, 76)
+        Me.btn_saludo.TabIndex = 0
+        Me.btn_saludo.Text = "Saludo"
+        Me.btn_saludo.UseVisualStyleBackColor = True
+        '
+        'btn_salir
+        '
+        Me.btn_salir.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_salir.Location = New System.Drawing.Point(34, 94)
+        Me.btn_salir.Name = "btn_salir"
+        Me.btn_salir.Size = New System.Drawing.Size(75, 26)
+        Me.btn_salir.TabIndex = 1
+        Me.btn_salir.Text = "Salir"
+        Me.btn_salir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(143, 196)
+        Me.Controls.Add(Me.btn_salir)
+        Me.Controls.Add(Me.btn_saludo)
+        Me.Name = "Form1"
+        Me.Text = "Hola Mundo"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btn_saludo As System.Windows.Forms.Button
+    Friend WithEvents btn_salir As System.Windows.Forms.Button
+
+End Class
---
+++ hola_mundo/hola_mundo/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    ' Subrutina de saludo
+    Private Sub btn_saludo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_saludo.Click
+        Dim nombre As String
+        nombre = InputBox("Introduce un nombre: ", "Nombre", "")
+        MsgBox("Hola holita " & nombre, MsgBoxStyle.OkOnly, "Saludos")
+    End Sub
+
+    ' Subrutina de salida del programa
+    Private Sub btn_salir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_salir.Click
+        End
+    End Sub
+End Class
