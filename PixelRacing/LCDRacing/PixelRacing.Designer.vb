c796a9a7ccabd1a545de19a279a37bf4ce857d5f
Alfonso Saavedra "Son Link <sonlink.dourden@gmail.com>
2015-10-14 20:08:50 +0200

    Añadido un mini juego de coches

---
+++ PixelRacing/LCDRacing/PixelRacing.Designer.vb
@@ -0,0 +1,314 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class PixelRacing
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(PixelRacing))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PanelGame = New System.Windows.Forms.Panel()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.labelScore = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LabelLives = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.pbExplosion = New System.Windows.Forms.PictureBox()
+        Me.ene1 = New System.Windows.Forms.PictureBox()
+        Me.ene3 = New System.Windows.Forms.PictureBox()
+        Me.pbPlayer = New System.Windows.Forms.PictureBox()
+        Me.ene2 = New System.Windows.Forms.PictureBox()
+        Me.pbBorder = New System.Windows.Forms.PictureBox()
+        Me.pbBG = New System.Windows.Forms.PictureBox()
+        Me.LabelGame = New System.Windows.Forms.Label()
+        Me.PanelGame.SuspendLayout()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbExplosion, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ene1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ene3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbPlayer, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ene2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbBorder, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbBG, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 70
+        '
+        'PanelGame
+        '
+        Me.PanelGame.Controls.Add(Me.LabelGame)
+        Me.PanelGame.Controls.Add(Me.pbExplosion)
+        Me.PanelGame.Controls.Add(Me.ene1)
+        Me.PanelGame.Controls.Add(Me.ene3)
+        Me.PanelGame.Controls.Add(Me.pbPlayer)
+        Me.PanelGame.Controls.Add(Me.ene2)
+        Me.PanelGame.Controls.Add(Me.pbBorder)
+        Me.PanelGame.Controls.Add(Me.pbBG)
+        Me.PanelGame.Controls.Add(Me.Label8)
+        Me.PanelGame.Controls.Add(Me.Label7)
+        Me.PanelGame.Controls.Add(Me.Label6)
+        Me.PanelGame.Controls.Add(Me.Label5)
+        Me.PanelGame.Controls.Add(Me.Label4)
+        Me.PanelGame.Location = New System.Drawing.Point(12, 12)
+        Me.PanelGame.Name = "PanelGame"
+        Me.PanelGame.Size = New System.Drawing.Size(208, 320)
+        Me.PanelGame.TabIndex = 0
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(214, 86)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(150, 39)
+        Me.Label8.TabIndex = 6
+        Me.Label8.Text = "How to play:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Left and Right keys: Move car" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "P: Play/Pause game"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(214, 73)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(32, 13)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "Lives"
+        '
+        'Label6
+        '
+        Me.Label6.Location = New System.Drawing.Point(262, 50)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(52, 23)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Tag = "label"
+        Me.Label6.Text = "4"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(214, 28)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(47, 13)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "    Score"
+        '
+        'Label4
+        '
+        Me.Label4.Location = New System.Drawing.Point(214, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(100, 23)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Tag = "label"
+        Me.Label4.Text = "0"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'labelScore
+        '
+        Me.labelScore.AutoSize = True
+        Me.labelScore.Location = New System.Drawing.Point(226, 35)
+        Me.labelScore.Name = "labelScore"
+        Me.labelScore.Size = New System.Drawing.Size(13, 13)
+        Me.labelScore.TabIndex = 1
+        Me.labelScore.Tag = "label"
+        Me.labelScore.Text = "0"
+        Me.labelScore.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Press Start 2P", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(226, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Score"
+        '
+        'LabelLives
+        '
+        Me.LabelLives.AutoSize = True
+        Me.LabelLives.Location = New System.Drawing.Point(226, 67)
+        Me.LabelLives.Name = "LabelLives"
+        Me.LabelLives.Size = New System.Drawing.Size(13, 13)
+        Me.LabelLives.TabIndex = 4
+        Me.LabelLives.Tag = "label"
+        Me.LabelLives.Text = "4"
+        Me.LabelLives.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(226, 53)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Lives:"
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(0, 0)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(75, 23)
+        Me.AxWindowsMediaPlayer1.TabIndex = 6
+        Me.AxWindowsMediaPlayer1.Visible = False
+        '
+        'pbExplosion
+        '
+        Me.pbExplosion.Location = New System.Drawing.Point(88, 247)
+        Me.pbExplosion.Name = "pbExplosion"
+        Me.pbExplosion.Size = New System.Drawing.Size(32, 32)
+        Me.pbExplosion.TabIndex = 29
+        Me.pbExplosion.TabStop = False
+        Me.pbExplosion.Visible = False
+        '
+        'ene1
+        '
+        Me.ene1.BackColor = System.Drawing.Color.Transparent
+        Me.ene1.Image = Global.WindowsApplication1.My.Resources.Resources.ene1
+        Me.ene1.Location = New System.Drawing.Point(41, 5)
+        Me.ene1.Name = "ene1"
+        Me.ene1.Size = New System.Drawing.Size(28, 52)
+        Me.ene1.TabIndex = 31
+        Me.ene1.TabStop = False
+        Me.ene1.Tag = "enemy"
+        Me.ene1.Visible = False
+        '
+        'ene3
+        '
+        Me.ene3.BackColor = System.Drawing.Color.Transparent
+        Me.ene3.Image = Global.WindowsApplication1.My.Resources.Resources.ene3
+        Me.ene3.Location = New System.Drawing.Point(137, 5)
+        Me.ene3.Name = "ene3"
+        Me.ene3.Size = New System.Drawing.Size(28, 52)
+        Me.ene3.TabIndex = 30
+        Me.ene3.TabStop = False
+        Me.ene3.Tag = "enemy"
+        Me.ene3.Visible = False
+        '
+        'pbPlayer
+        '
+        Me.pbPlayer.BackColor = System.Drawing.Color.Transparent
+        Me.pbPlayer.Image = CType(resources.GetObject("pbPlayer.Image"), System.Drawing.Image)
+        Me.pbPlayer.Location = New System.Drawing.Point(90, 261)
+        Me.pbPlayer.Name = "pbPlayer"
+        Me.pbPlayer.Size = New System.Drawing.Size(28, 52)
+        Me.pbPlayer.TabIndex = 28
+        Me.pbPlayer.TabStop = False
+        '
+        'ene2
+        '
+        Me.ene2.BackColor = System.Drawing.Color.Transparent
+        Me.ene2.Image = Global.WindowsApplication1.My.Resources.Resources.ene2
+        Me.ene2.Location = New System.Drawing.Point(89, 5)
+        Me.ene2.Name = "ene2"
+        Me.ene2.Size = New System.Drawing.Size(28, 52)
+        Me.ene2.TabIndex = 27
+        Me.ene2.TabStop = False
+        Me.ene2.Tag = "enemy"
+        Me.ene2.Visible = False
+        '
+        'pbBorder
+        '
+        Me.pbBorder.Image = Global.WindowsApplication1.My.Resources.Resources.border
+        Me.pbBorder.Location = New System.Drawing.Point(0, -96)
+        Me.pbBorder.Name = "pbBorder"
+        Me.pbBorder.Size = New System.Drawing.Size(208, 416)
+        Me.pbBorder.TabIndex = 26
+        Me.pbBorder.TabStop = False
+        '
+        'pbBG
+        '
+        Me.pbBG.BackColor = System.Drawing.Color.Transparent
+        Me.pbBG.Image = Global.WindowsApplication1.My.Resources.Resources.bg
+        Me.pbBG.Location = New System.Drawing.Point(0, 0)
+        Me.pbBG.Name = "pbBG"
+        Me.pbBG.Size = New System.Drawing.Size(208, 320)
+        Me.pbBG.TabIndex = 25
+        Me.pbBG.TabStop = False
+        '
+        'LabelGame
+        '
+        Me.LabelGame.BackColor = System.Drawing.Color.White
+        Me.LabelGame.ForeColor = System.Drawing.Color.Black
+        Me.LabelGame.Location = New System.Drawing.Point(0, 161)
+        Me.LabelGame.Name = "LabelGame"
+        Me.LabelGame.Size = New System.Drawing.Size(208, 23)
+        Me.LabelGame.TabIndex = 32
+        Me.LabelGame.Text = "Press P to start game"
+        Me.LabelGame.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PixelRacing
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(301, 348)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.LabelLives)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.labelScore)
+        Me.Controls.Add(Me.PanelGame)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "PixelRacing"
+        Me.Text = "PixelRacing"
+        Me.PanelGame.ResumeLayout(False)
+        Me.PanelGame.PerformLayout()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbExplosion, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ene1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ene3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbPlayer, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ene2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbBorder, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbBG, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents PanelGame As System.Windows.Forms.Panel
+    Friend WithEvents labelScore As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents LabelLives As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents pbBG As System.Windows.Forms.PictureBox
+    Friend WithEvents pbExplosion As System.Windows.Forms.PictureBox
+    Friend WithEvents ene1 As System.Windows.Forms.PictureBox
+    Friend WithEvents ene3 As System.Windows.Forms.PictureBox
+    Friend WithEvents pbPlayer As System.Windows.Forms.PictureBox
+    Friend WithEvents ene2 As System.Windows.Forms.PictureBox
+    Friend WithEvents pbBorder As System.Windows.Forms.PictureBox
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents LabelGame As System.Windows.Forms.Label
+
+End Class
---
+++ PixelRacing/LCDRacing/PixelRacing.vb
@@ -0,0 +1,240 @@
+﻿Public Class PixelRacing
+    Dim playerPos As Integer = 1
+    Dim explosionPos As Integer = 48
+    Dim cont As Integer = 0
+    Dim score As Integer = 0
+    Dim hitScore As Integer
+    Dim ifHitScore As Boolean = False
+    Dim lives As Integer = 4
+    Dim level As Integer = 1
+    Dim block As Boolean = False
+    Dim speed As Integer = 70
+    Dim upSpeed As Integer = 2000
+    Dim newLiveAt As Integer = 5000
+    Dim gameState As Integer = 0 ' 0: no play, 1: play/pause, 2: game over
+    Private Declare Function GetTickCount Lib "kernel32" () As Integer
+
+    Private Sub Form1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        If Not block Then
+            If e.KeyCode = Keys.Left And pbPlayer.Location.X > 36 Then
+                pbPlayer.Location = New Point(pbPlayer.Location.X - 6, 256)
+            ElseIf e.KeyCode = Keys.Right And pbPlayer.Location.X < 142 Then
+                pbPlayer.Location = New Point(pbPlayer.Location.X + 6, 256)
+            ElseIf e.KeyCode = Keys.P Then
+                If gameState = 1 Then
+                    pauseResume()
+                ElseIf gameState = 0 Then
+                    StartGame()
+                    gameState = 1
+                End If
+            ElseIf e.KeyCode = Keys.Y And gameState = 2 Then
+                StartGame()
+                gameState = 0
+            ElseIf e.KeyCode = Keys.N And gameState = 2 Then
+                End
+            End If
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim privateFonts As New System.Drawing.Text.PrivateFontCollection()
+        privateFonts.AddFontFile("PixelOperator.ttf")
+        For Each label In Me.Controls
+            If TypeOf label Is Label Then
+                label.Font = New Font(privateFonts.Families(0), 14, FontStyle.Regular)
+            End If
+        Next
+        LabelGame.Font = New Font(privateFonts.Families(0), 14, FontStyle.Regular)
+        pbPlayer.Location = New Point(90, 256)
+        Try
+            FileOpen(1, "hitscore.txt", OpenMode.Input)
+            hitScore = Val(LineInput(1))
+            FileClose(1)
+        Catch ex As Exception
+            hitScore = 0
+        End Try
+        pbBG.Controls.Add(pbPlayer)
+        pbBG.Controls.Add(pbBorder)
+        pbBG.Controls.Add(ene1)
+        pbBG.Controls.Add(ene2)
+        pbBG.Controls.Add(ene3)
+        pbBG.Controls.Add(pbExplosion)
+        ene1.BringToFront()
+        ene2.BringToFront()
+        ene3.BringToFront()
+        pbExplosion.BringToFront()
+    End Sub
+
+    Private Sub StartGame()
+        pbPlayer.Location = New Point(90, 256)
+        pbPlayer.Show()
+        lives = 4
+        LabelLives.Text = 4
+        LabelGame.Hide()
+        resetGame()
+        AxWindowsMediaPlayer1.URL = "bg_music.mp3"
+        AxWindowsMediaPlayer1.settings.setMode("loop", True)
+        'newEnemy()
+        Timer1.Enabled = True
+        Timer1.Start()
+
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If cont = 20 Then
+            cont = 0
+            newEnemy()
+        Else
+            cont = cont + 1
+        End If
+        For Each enemy In pbBG.Controls
+            If TypeOf enemy Is PictureBox Then
+                If enemy.Tag = "enemy" Then
+                    If enemy.Location.Y < 320 Then
+                        enemy.Location = New Point(enemy.Location.X, enemy.Location.Y + 8)
+                    Else
+                        score = score + 100
+                        labelScore.Text = score
+                        enemy.Location = New Point(16, 0)
+                        enemy.Visible = False
+                        If score > hitScore Then
+                            Label1.Text = "Hit score"
+                            ifHitScore = True
+                        End If
+                        If score = upSpeed And Timer1.Interval > 20 Then
+                            Timer1.Interval = Timer1.Interval - 10
+                            upSpeed = upSpeed + 2000
+                        End If
+                        If score = newLiveAt Then
+                            lives = lives + 1
+                            LabelLives.Text = lives
+                            newLiveAt = newLiveAt + 5000
+                        End If
+                    End If
+                End If
+                Me.Refresh()
+            End If
+        Next
+
+        For Each PictureBox In pbBG.Controls
+            If PictureBox IsNot pbPlayer AndAlso pbPlayer.Bounds.IntersectsWith(PictureBox.Bounds) And PictureBox IsNot pbExplosion And PictureBox IsNot pbBorder And PictureBox.Visible Then
+                If lives > 0 Then
+                    block = True
+                    Timer1.Stop()
+                    Timer1.Enabled = False
+                    pbExplosion.Location = New Point(pbPlayer.Location.X - 2, pbPlayer.Location.Y - 16)
+                    pbPlayer.Visible = False
+                    PictureBox.Visible = False
+                    pbExplosion.Visible = True
+                    pbExplosion.Image = My.Resources.explosion_ani
+                    My.Computer.Audio.Play(My.Resources.explosion, AudioPlayMode.Background)
+                    esperar(2000)
+                    cont = 0
+                    lives = lives - 1
+                    LabelLives.Text = lives
+                    resetGame()
+                    Exit Sub
+                Else
+                    Timer1.Stop()
+                    Timer1.Enabled = False
+                    If ifHitScore Then
+                        FileOpen(1, "hitscore.txt", OpenMode.Output)
+                        WriteLine(1, score)
+                        FileClose(1)
+                    End If
+                    pbExplosion.Location = New Point(pbPlayer.Location.X - 2, pbPlayer.Location.Y - 16)
+                    pbPlayer.Visible = False
+                    PictureBox.Visible = False
+                    pbExplosion.Visible = True
+                    pbExplosion.Image = My.Resources.explosion_ani
+                    My.Computer.Audio.Play(My.Resources.explosion, AudioPlayMode.Background)
+                    esperar(2000)
+                    pbExplosion.Visible = False
+                    AxWindowsMediaPlayer1.Ctlcontrols.stop()
+                    My.Computer.Audio.Play(My.Resources.game_over, AudioPlayMode.Background)
+                    gameState = 2
+                    LabelGame.Text = "GAME OVER! Continue? Yes/No"
+                    LabelGame.Show()
+                End If
+            End If
+        Next
+        If pbBorder.Location.Y <= -4 Then
+            pbBorder.Location = New Point(0, pbBorder.Location.Y + 4)
+        Else
+            pbBorder.Location = New Point(0, -96)
+        End If
+    End Sub
+
+    Private Sub newEnemy()
+        Randomize()
+        Dim pos = CInt(2 * Rnd())
+        Dim x = 0
+        If pos = 0 Then
+            x = 41
+        ElseIf pos = 1 Then
+            x = 90
+        Else
+            x = 138
+        End If
+        For Each enemy In pbBG.Controls
+            If TypeOf enemy Is PictureBox Then
+                If enemy.Tag = "enemy" And Not enemy.Visible Then
+                    Randomize()
+                    Dim color As Integer = CInt(2 * Rnd())
+                    If color = 0 Then
+                        enemy.Image = My.Resources.ene1
+                    ElseIf color = 1 Then
+                        enemy.Image = My.Resources.ene2
+                    Else
+                        enemy.Image = My.Resources.ene3
+                    End If
+                    enemy.Visible = True
+                    enemy.Location = New System.Drawing.Point(x, -48)
+                    Me.Refresh()
+                    Exit Sub
+                End If
+            End If
+        Next
+    End Sub
+
+    Private Sub pauseResume()
+        If Timer1.Enabled Then
+            Timer1.Stop()
+            Timer1.Enabled = False
+            AxWindowsMediaPlayer1.Ctlcontrols.pause()
+        Else
+            Timer1.Start()
+            Timer1.Enabled = True
+            AxWindowsMediaPlayer1.Ctlcontrols.play()
+        End If
+    End Sub
+
+    Private Sub resetGame()
+        cont = 0
+        pbExplosion.Visible = False
+        pbPlayer.Location = New Point(90, 256)
+        explosionPos = 48
+        ene1.Location = New Point(-32, -48)
+        ene2.Location = New Point(-32, -48)
+        ene3.Location = New Point(-32, -48)
+        ene1.Visible = False
+        ene2.Visible = False
+        ene3.Visible = False
+        pbPlayer.Visible = True
+        esperar(2000)
+        Me.Refresh()
+        block = False
+        AxWindowsMediaPlayer1.Ctlcontrols.play()
+        newEnemy()
+        Timer1.Enabled = True
+        Timer1.Start()
+    End Sub
+
+    Private Sub esperar(ByVal time_whait As Integer)
+        Dim retraso As Integer
+        retraso = time_whait + GetTickCount
+        While retraso >= GetTickCount
+            Application.DoEvents()
+        End While
+    End Sub
+End Class
